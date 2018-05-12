d89ae055be5a933ba07dc2ec44375820f00c8eb3
ahzaab <ahzaab@outlook.com>
2018-05-12 10:22:06 -0400

    Fixed a bug that was exploited by MO2

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ SKSEScaleformInterface		* g_scaleform = NULL;
 SKSEMessagingInterface *g_skseMessaging = NULL;
 AHZEventHandler menuEvent;
 AHZCrosshairRefEventHandler crossHairEvent;
-#define PLUGIN_VERSION  (30407)
+#define PLUGIN_VERSION  (30408)
 
 // Just initialize to start routing to the console window
 CAHZDebugConsole theDebugConsole;
