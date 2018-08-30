f5f3d206d8a9a96ffc43a697b5c77eae0f8c9aea
ahzaab <ahzaab@outlook.com>
2018-08-30 14:14:47 -0400

    1. Fixed a bug that prevented some HUD mods from being able to display V/W
      2. Supports flora, trees, that have less than 4 ingredients

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ SKSEScaleformInterface		* g_scaleform = NULL;
 SKSEMessagingInterface *g_skseMessaging = NULL;
 AHZEventHandler menuEvent;
 AHZCrosshairRefEventHandler crossHairEvent;
-#define PLUGIN_VERSION  (30500)
+#define PLUGIN_VERSION  (30501)
 
 // Just initialize to start routing to the console window
 CAHZDebugConsole theDebugConsole;
