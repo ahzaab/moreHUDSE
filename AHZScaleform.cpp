ded92400ed52e6412194abddc13bb1ddeed2e4e1
ahzaab <ahzaab@outlook.com>
2018-09-23 15:49:44 -0400

    Added a patch to SKSE to release all game implemented locks.  This would cause a deadlock for SSE Engine fixes because it expected the Games Lock to be called.
      Improved the performance for getting the Value to Weight Ratio by removing the need for the slow regex

--- AHZScaleform.cpp
+++ AHZScaleform.cpp
@@ -1831,75 +1831,66 @@ string CAHZScaleform::GetArmorWeightClass(TESObjectREFR *theObject)
 
 string CAHZScaleform::GetValueToWeight(TESObjectREFR *theObject, const char * stringFromHUD, const char * vmTranslated)
 {
-   string desc;
+	string desc;
 
-   if (!theObject)
-      return desc;
+	if (!theObject)
+		return desc;
 
-   if (!theObject->baseForm)
-      return desc;
+	if (!theObject->baseForm)
+		return desc;
 
-   if (!stringFromHUD)
-      return desc;
+	if (!stringFromHUD)
+		return desc;
 
-   //<TEXTFORMAT INDENT="0" LEFTMARGIN="0" RIGHTMARGIN="0" LEADING="2"><P ALIGN="CENTER"><FONT FACE="$EverywhereMediumFont" SIZE="15" COLOR="#999999" KERNING="0">WEIGHT </FONT><FONT FACE="$EverywhereBoldFont" SIZE="24" COLOR="#FFFFFF" KERNING="0">0.5</FONT><FONT FACE="$EverywhereMediumFont" SIZE="15" COLOR="#999999" KERNING="0">      VALUE </FONT><FONT FACE="$EverywhereBoldFont" SIZE="24" COLOR="#FFFFFF" KERNING="0">21</FONT></P></TEXTFORMAT>
+	char *end;
+	float weightValue = 0.0;
+	float valueValue = 0.0;
+	TESWeightForm* pWeight = DYNAMIC_CAST(theObject->baseForm, TESForm, TESWeightForm);
+	if (pWeight)
+		weightValue = pWeight->weight;
+	TESValueForm* pValue = DYNAMIC_CAST(theObject->baseForm, TESForm, TESValueForm);
+	if (pValue)
+		valueValue = pValue->value;
+	else {
+		AlchemyItem* alchemyItem = DYNAMIC_CAST(theObject->baseForm, TESForm, AlchemyItem);
+		if (alchemyItem && (alchemyItem->itemData.flags & AlchemyItem::kFlag_ManualCalc) == AlchemyItem::kFlag_ManualCalc)
+			valueValue = alchemyItem->itemData.value;
+	}
 
-   // Using regex from the HUD string to extract the value and weight values.  The SKSE version are either broken or unreliable
-   std::regex rgx(R"(^(<TEXTFORMAT INDENT="0" LEFTMARGIN="0" RIGHTMARGIN="0" LEADING="2"><P ALIGN="CENTER"><FONT FACE="\$EverywhereMediumFont" SIZE="\d.+" COLOR="#999999" KERNING="0">)([\W\w\s]+)(?:<\/FONT><FONT FACE="\$EverywhereBoldFont" SIZE="\d.+" COLOR="#FFFFFF" KERNING="0">)((?:[0-9]+\.[0-9]*)|(?:[0-9]*\.[0-9]+)|(?:[0-9]+))(?:<\/FONT><FONT FACE="\$EverywhereMediumFont" SIZE="\d.+" COLOR="#999999" KERNING="0">)([\W\w\s]+)(?:<\/FONT><FONT FACE="\$EverywhereBoldFont" SIZE="\d.+" COLOR="#FFFFFF" KERNING="0">)((?:[0-9]+\.[0-9]*)|(?:[0-9]*\.[0-9]+)|(?:[0-9]+))(<\/FONT>([\w\s]+|)<\/P><\/TEXTFORMAT>)$)");
-   std::smatch match;
-   string s = stringFromHUD;
-   const string cs = const_cast<string &>(s);
+	// Don't show a neg or 0 ratio, its pointless
+	if ((mRound(weightValue * 100) / 100) <= 0.0 || (mRound(valueValue * 100) / 100) <= 0.0)
+	{
+		return desc;
+	}
 
-   if (regex_search(cs.begin(), cs.end(), match, rgx))
-   {
-      if (match.size() < 7)
-      {
-         return desc;
-      }
+	float vW = valueValue / weightValue;
 
-      // The fixed positions of the matches (containing groups)
-      string weight = match[3];
-      string value = match[5];
-      char *end;
+	// Add the VW label
+	desc.append("<FONT FACE=\"$EverywhereMediumFont\"SIZE=\"15\"COLOR=\"#999999\"KERNING=\"0\">     ");
+	desc.append(vmTranslated);
+	desc.append(" <\\FONT>");
 
-      float weightValue = strtof(weight.c_str(), &end);
-      float valueValue = strtof(value.c_str(), &end);
+	char floatHold[64];
+	size_t size = 64;
 
-      // Don't show a neg or 0 ratio, its pointless
-      if (weightValue <= 0.0 || valueValue <= 0.0)
-      {
-         return desc;
-      }
+	//Rounding trick
+	sprintf_s(floatHold, size, "%.2f", vW);
+	vW = strtof(floatHold, &end);
 
-      float vW = valueValue / weightValue;
+	if (vW < 1.0)
+	{
+		sprintf_s(floatHold, size, "%.1g", vW);
+	}
+	else
+	{
+		sprintf_s(floatHold, size, "%.0f", vW);
+	}
 
-      // Add the VW label
-      desc.append("<FONT FACE=\"$EverywhereMediumFont\"SIZE=\"15\"COLOR=\"#999999\"KERNING=\"0\">     ");
-      desc.append(vmTranslated);
-      desc.append(" <\\FONT>");
-
-      char floatHold[64];
-      size_t size = 64;
-
-      //Rounding trick
-      sprintf_s(floatHold, size, "%.2f", vW);
-      vW = strtof(floatHold, &end);
-
-      if (vW < 1.0)
-      {
-         sprintf_s(floatHold, size, "%.1g", vW);
-      }
-      else
-      {
-         sprintf_s(floatHold, size, "%.0f", vW);
-      }
-
-      desc.append("<FONT FACE=\"$EverywhereBoldFont\"SIZE=\"24\"COLOR=\"#FFFFFF\"KERNING=\"0\">");
-      desc.append(floatHold);
-      desc.append("<\\FONT>");
-   }
-
-   return desc;
+	desc.append("<FONT FACE=\"$EverywhereBoldFont\"SIZE=\"24\"COLOR=\"#FFFFFF\"KERNING=\"0\">");
+	desc.append(floatHold);
+	desc.append("<\\FONT>");
+   
+	return desc;
 };
 
 string CAHZScaleform::GetBookSkill(TESObjectREFR *theObject)
--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ SKSEScaleformInterface		* g_scaleform = NULL;
 SKSEMessagingInterface *g_skseMessaging = NULL;
 AHZEventHandler menuEvent;
 AHZCrosshairRefEventHandler crossHairEvent;
-#define PLUGIN_VERSION  (30503)
+#define PLUGIN_VERSION  (30504)
 
 // Just initialize to start routing to the console window
 CAHZDebugConsole theDebugConsole;
